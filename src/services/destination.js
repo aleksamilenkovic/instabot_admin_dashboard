c59985ae5ab0b5df07fbe077fc8f42f3dd4d8056
Aleksa Milenkovic <amilenko@cisco.com>
2021-12-24 15:49:27 +0100

    package.json scripts

--- src/services/destination.js
+++ src/services/destination.js
@@ -1,5 +1,6 @@
-export const API ={
-    BASEURL : 'http://localhost:8086/api/',
+export const API = {
+    BASEURL : 'https://instabot-admin-service.herokuapp.com/api/',
+    // BASEURL : 'http://localhost:8086/api/',
     // BASEURL : 'http://192.168.182.123:8086/api/',
     SIGNIN : 'auth/signin',
     REGISTER : 'auth/register',
